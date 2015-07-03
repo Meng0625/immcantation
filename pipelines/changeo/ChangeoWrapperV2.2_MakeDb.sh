a389d9340f8f928f21c23ea3c3975af207ed7ec7
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2015-07-03 13:10:34 -0400

    Fixed typo in changeo pipeline script.

--- pipelines/changeo/ChangeoWrapperV2.2_MakeDb.sh
+++ pipelines/changeo/ChangeoWrapperV2.2_MakeDb.sh
@@ -15,7 +15,7 @@ do
     BASENAME=$(basename "${FILE_ARRAY[0]}" ".zip")
     MakeDb.py imgt -i "${FILE_ARRAY[0]}" -s "${FILE_ARRAY[1]}" --outdir $2
     ParseDb.py select -d "$2/${BASENAME}_db-pass.tab" -f FUNCTIONAL -u T \
-        --outname "${BASENAME}_functional.tab"
+        --outname "${BASENAME}_functional"
     ParseDb.py select -d "$2/${BASENAME}_functional_parse-select.tab" -f V_CALL J_CALL \
         -u "IGH" --logic all --regex --outname "${BASENAME}_functional-heavy"
     ParseDb.py select -d "$2/${BASENAME}_functional_parse-select.tab" -f V_CALL J_CALL \
