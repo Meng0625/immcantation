347eb72e5a28d255d1a342eacb77711b19948442
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2015-07-08 16:35:19 -0400

    Updated changeo pipeline scripts.

--- pipelines/changeo/RunChangeoV2.2_MakeDb.sh
+++ pipelines/changeo/RunChangeoV2.2_MakeDb.sh
@@ -2,7 +2,7 @@
 # Script to run MakeDb and ParseDb on multiple inputs
 # 
 # Author:  Jason Anthony Vander Heiden
-# Date:    2015.07.03
+# Date:    2015.07.08
 #
 # Required Arguments:
 #   $1 = A two column tab delimited file mapping IMGT zip files (column 1) 
@@ -17,14 +17,14 @@ do
     FILE_ARRAY=($FILE_MAP)
     BASENAME=$(basename "${FILE_ARRAY[0]}" ".zip")
     MakeDb.py imgt -i "${FILE_ARRAY[0]}" -s "${FILE_ARRAY[1]}" --outdir $2/db
-    ParseDb.py select -d "$2/${BASENAME}_db-pass.tab" -f FUNCTIONAL -u T \
+    ParseDb.py select -d $2/db/"${BASENAME}_db-pass.tab" -f FUNCTIONAL -u T \
         --outname "${BASENAME}_functional" --outdir $2/db
-    ParseDb.py select -d "$2/${BASENAME}_functional_parse-select.tab" -f V_CALL J_CALL \
+    ParseDb.py select -d $2/db/"${BASENAME}_functional_parse-select.tab" -f V_CALL J_CALL \
         -u "IGH" --logic all --regex --outname "${BASENAME}_functional-heavy" \
         --outdir $2/functional
-    ParseDb.py select -d "$2/${BASENAME}_functional_parse-select.tab" -f V_CALL J_CALL \
+    ParseDb.py select -d $2/db/"${BASENAME}_functional_parse-select.tab" -f V_CALL J_CALL \
         -u "IG[LK]" --logic all --regex --outname "${BASENAME}_functional-light" \
         --outdir $2/functional
-    rm "$2/db/${BASENAME}_functional_parse-select.tab"
+    rm $2/db/"${BASENAME}_functional_parse-select.tab"
 done < $1
 
