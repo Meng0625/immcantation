b955cd17c0f2a45652af8c890d83af287bfea365
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2017-01-31 12:26:00 -0500

    Minor.

--- docs/conf.py
+++ docs/conf.py
@@ -30,8 +30,7 @@ needs_sphinx = '1.4'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.imgmath',
-              'sphinx.ext.intersphinx',
+extensions = ['sphinx.ext.intersphinx',
               'sphinx.ext.todo']
 
 # Add any paths that contain templates here, relative to this directory.
