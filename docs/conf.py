52eb55520ec1a722b8c6ad5561887f505df10733
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2017-08-04 15:54:45 -0400

    Moved intersphinx mappings to subproject urls.

--- docs/conf.py
+++ docs/conf.py
@@ -290,8 +290,8 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'python': ('https://docs.python.org/3', None),
-                       'presto': ('https://presto.readthedocs.io', None),
-                       'changeo': ('https://changeo.readthedocs.io', None),
-                       'alakazam': ('https://alakazam.readthedocs.io', None),
-                       'shazam': ('https://shazam.readthedocs.io', None),
-                       'tigger': ('https://tigger.readthedocs.io', None)}
+                       'presto': ('https://immcantation.readthedocs.io/projects/presto', None),
+                       'changeo': ('https://immcantation.readthedocs.io/projects/changeo', None),
+                       'alakazam': ('https://immcantation.readthedocs.io/projects/alakazam', None),
+                       'shazam': ('https://immcantation.readthedocs.io/projects/shazam', None),
+                       'tigger': ('https://immcantation.readthedocs.io/projects/tigger', None)}
