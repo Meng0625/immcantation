00cd3859926767ea8fd23ca469592430542993e6
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2017-08-04 15:39:54 -0400

    Added intersphinx mappings to doc conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -289,4 +289,9 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None),
+                       'presto': ('https://presto.readthedocs.io', None),
+                       'changeo': ('https://changeo.readthedocs.io', None),
+                       'alakazam': ('https://alakazam.readthedocs.io', None),
+                       'shazam': ('https://shazam.readthedocs.io', None),
+                       'tigger': ('https://tigger.readthedocs.io', None)}
