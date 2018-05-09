2e1212138a4bbc7d0d91b4b566b362cbacda3b62
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2018-05-09 08:39:02 -0400

    Updated singularity tests.

--- tests/test_singularity.sh
+++ tests/test_singularity.sh
@@ -1,63 +1,59 @@
 #!/usr/bin/env bash
+
 # Run parameters
 DATE=$(date +"%Y.%m.%d")
-DATA_DIR=/ysm-gpfs/pi/kleinstein/share/singularity/test/data
-SAMPLE_NAME=HD13M
+DATA_DIR="/ysm-gpfs/pi/kleinstein/share/singularity/tests/data"
+RUN_DIR="/ysm-gpfs/pi/kleinstein/share/singularity/tests/run/${DATE}"
+SAMPLE=HD13M
 NPROC=8
-IMAGE=/ysm-gpfs/pi/kleinstein/share/singularity/immcantation-devel.img
+IMAGE=/ysm-gpfs/pi/kleinstein/share/singularity/immcantation-devel-2018.05.07.img
 EXT="tab"
 
 # Create output parent
-mkdir -p run/${DATE}
+mkdir -p $RUN_DIR
 
 # PhiX
 READS_R1=/data/AAYHL_HD13M/MG2v3_HD13M_BC13_AGTCAA_L001_R1_001.fastq
 READS_R2=/data/AAYHL_HD13M/MG2v3_HD13M_BC13_AGTCAA_L001_R2_001.fastq
-OUT_DIR="/data/run/${DATE}/phix/${SAMPLE_NAME}"
+OUT_DIR="/scratch/phix"
 
-singularity exec -e -B $DATA_DIR:/data $IMAGE preprocess-phix \
-	-s $READS_R1 -o $OUT_DIR -p $NPROC \
-	| tee run/${DATE}/run_phix_1.out
+singularity exec -e -B $DATA_DIR:/data -B $RUN_DIR:/scratch $IMAGE preprocess-phix \
+	-s $READS_R1 -o $OUT_DIR -p $NPROC
 	
 # pRESTO
 READS_R1=/data/AAYHL_HD13M/MG2v3_HD13M_BC13_AGTCAA_L001_R1_001.fastq
 READS_R2=/data/AAYHL_HD13M/MG2v3_HD13M_BC13_AGTCAA_L001_R2_001.fastq
 YAML=/data/report.yaml
-OUT_DIR="/data/run/${DATE}/presto/${SAMPLE_NAME}"
+OUT_DIR="/scratch/presto"
 
-docker run -it -v $DATA_DIR:/data:z $IMAGE presto-abseq \
-    -1 $READS_R1 -2 $READS_R2 -y $YAML -n $SAMPLE_NAME -o $OUT_DIR -p $NPROC \
-    | tee run/${DATE}/run_presto.out
+singularity exec -e -B $DATA_DIR:/data -B $RUN_DIR:/scratch $IMAGE presto-abseq \
+    -1 $READS_R1 -2 $READS_R2 -y $YAML -n $SAMPLE -o $OUT_DIR -p $NPROC
 
 # IgBLAST
-READS="/data/run/${DATE}/presto/${SAMPLE_NAME}/${SAMPLE_NAME}-final_collapse-unique_atleast-2.fastq"
-OUT_DIR="/data/run/${DATE}/changeo/${SAMPLE_NAME}"
+READS="/scratch/presto/${SAMPLE}-final_collapse-unique_atleast-2.fastq"
+OUT_DIR="/scratch/changeo"
 
-singularity exec -B $DATA_DIR:/data $IMAGE changeo-igblast \
-    -s $READS -n $SAMPLE_NAME -o $OUT_DIR -p $NPROC \
-    | tee run/${DATE}/run_igblast.out
+singularity exec -B $DATA_DIR:/data -B $RUN_DIR:/scratch $IMAGE changeo-igblast \
+    -s $READS -n $SAMPLE -o $OUT_DIR -p $NPROC
 
 # TIgGER
-DB="/data/run/${DATE}/changeo/${SAMPLE_NAME}/${SAMPLE_NAME}_db-pass.${EXT}"
-OUT_DIR="/data/run/${DATE}/changeo/${SAMPLE_NAME}"
+DB="/scratch/changeo/${SAMPLE}_db-pass.${EXT}"
+OUT_DIR="/scratch/changeo"
 
-singularity exec -B $DATA_DIR:/data $IMAGE tigger-genotype \
-    -d $DB -n $SAMPLE_NAME -o $OUT_DIR -p $NPROC \
-    | tee run/${DATE}/run_genotype.out
+singularity exec -B $DATA_DIR:/data -B $RUN_DIR:/scratch $IMAGE tigger-genotype \
+    -d $DB -n $SAMPLE -o $OUT_DIR -p $NPROC
 
 # SHazaM threshold
-DB="/data/run/${DATE}/changeo/${SAMPLE_NAME}/${SAMPLE_NAME}_genotyped.${EXT}"
-OUT_DIR="/data/run/${DATE}/changeo/${SAMPLE_NAME}"
+DB="/scratch/changeo/${SAMPLE}_genotyped.${EXT}"
+OUT_DIR="/scratch/changeo"
 
-singularity exec -B $DATA_DIR:/data $IMAGE shazam-threshold \
-	-d $DB -n $SAMPLE_NAME -o $OUT_DIR -p $NPROC \
-	| tee run/${DATE}/run_threshold.out
+singularity exec -B $DATA_DIR:/data -B $RUN_DIR:/scratch $IMAGE shazam-threshold \
+	-d $DB -n $SAMPLE -o $OUT_DIR -p $NPROC
 
 # Change-O clones
-DB="/data/run/${DATE}/changeo/${SAMPLE_NAME}/${SAMPLE_NAME}_genotyped.${EXT}"
-OUT_DIR="/data/run/${DATE}/changeo/${SAMPLE_NAME}"
+DB="/scratch/changeo/${SAMPLE}_genotyped.${EXT}"
+OUT_DIR="/scratch/changeo"
 DIST=0.15
 
-singularity exec -B $DATA_DIR:/data  $IMAGE changeo-clone \
-	-d $DB -x $DIST -n $SAMPLE_NAME -o $OUT_DIR -p $NPROC \
-	| tee run/${DATE}/run_clone.out
+singularity exec -B $DATA_DIR:/data -B $RUN_DIR:/scratch $IMAGE changeo-clone \
+	-d $DB -x $DIST -n $SAMPLE -o $OUT_DIR -p $NPROC
