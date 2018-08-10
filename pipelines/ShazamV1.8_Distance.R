46ed1479a04858cdad37044b0b194ceb71c6dbd8
ruoyijiangyale <ruoyi.jiang@yale.edu>
2018-08-09 21:01:44 -0400

    DTN with minimal bootstrapping

--- pipelines/ShazamV1.8_Distance.R
+++ pipelines/ShazamV1.8_Distance.R
@@ -16,7 +16,9 @@
 #                Defaults to the available processing units.
 #   --model      Model when "-m gmm" is specified.
 #                Defaults to "gamma-gamma".
-#   --subsample  Number of distances to downsample the data to before distance calculation.
+#   --subsample  Number of rows to downsample the data to before distance calculation.
+#   --tsubsample Number of distances to downsample the data to before threshold calculation.
+#   --repeats    Number of times to repeat the threshold calculation (with plotting)
 #   -h           Display help.
 
 # Imports
@@ -29,30 +31,42 @@ suppressPackageStartupMessages(library("alakazam"))
 suppressPackageStartupMessages(library("shazam"))
 
 # Set defaults
+METHOD <- "density"
+OUTDIR <- "."
+MODEL <- "gamma-gamma"
 NPROC <- parallel::detectCores()
 SUBSAMPLE <- 15000
+TSUBSAMPLE <- 500
+REPEATS <- 5
 
 # Define commmandline arguments
 opt_list <- list(make_option(c("-d", "--db"), dest="DB",
                              help="Change-O formatted TSV (TAB) file."),
-                 make_option(c("-m", "--method"), dest="METHOD", default="density",
+                 make_option(c("-m", "--method"), dest="METHOD", default=METHOD,
                              help=paste("Threshold inferrence to use. One of gmm or density.",
                                         "\n\t\tDefaults to density.")),
                  make_option(c("-n", "--name"), dest="NAME",
                              help=paste("Sample name or run identifier which will be used as the output file prefix.",
                                         "\n\t\tDefaults to a truncated version of the input filename.")),
-                 make_option(c("-o", "--outdir"), dest="OUTDIR", default=".",
+                 make_option(c("-o", "--outdir"), dest="OUTDIR", default=OUTDIR,
                              help=paste("Output directory.", "Defaults to the sample name.")),
                  make_option(c("-p", "--nproc"), dest="NPROC", default=NPROC,
                              help=paste("Number of subprocesses for multiprocessing tools.",
                                         "\n\t\tDefaults to the available processing units.")),
-                 make_option(c("--model"), dest="MODEL", default="gamma-gamma",
+                 make_option(c("--model"), dest="MODEL", default=MODEL,
                              help=paste("Model to use for the gmm model.",
                                         "\n\t\tOne of gamma-gamma, gamma-norm, norm-norm or norm-gamma.",
                                         "\n\t\tDefaults to gamma-gamma.")),
                  make_option(c("--subsample"), dest="SUBSAMPLE", default=SUBSAMPLE,
                              help=paste("Number of records to downsample the data to before distance calculation.",
                                         "\n\t\tDefaults to 15000.")))
+                 make_option(c("--tsubsample"), dest="TSUBSAMPLE", default=TSUBSAMPLE,
+                             help=paste("Number of distances to downsample the data to before threshold calculation.",
+                                        "\n\t\tDefaults to 500.")))
+                 make_option(c("--repeats"), dest="REPEATS", default=REPEATS,
+                             help=paste("Number of times to recalculate.",
+                                        "\n\t\tDefaults to 5.")))
+                
 # Parse arguments
 opt <- parse_args(OptionParser(option_list=opt_list))
 
@@ -72,30 +86,58 @@ if (!(dir.exists(opt$OUTDIR))) {
     dir.create(opt$OUTDIR)
 }
 
+# Reset parameters from opt (better for debugging)
+DB <- opt$DB
+METHOD <- opt$METHOD
+OUTDIR <- opt$OUTDIR
+MODEL <- opt$MODEL
+NPROC <- opt$NPROC
+NAME <- opt$NAME
+SUBSAMPLE <- opt$SUBSAMPLE
+TSUBSAMPLE <- opt$TSUBSAMPLE
+REPEATS <- opt$REPEATS
+
+
+
 # Load data
-db <- as.data.frame(readChangeoDb(opt$DB))
-if (opt$SUBSAMPLE < nrow(db)) {
-    db <- db[sample(nrow(db), opt$SUBSAMPLE), ]
+db <- as.data.frame(readChangeoDb(DB))
+if (SUBSAMPLE < nrow(db)) {
+    db <- db[sample(nrow(db), SUBSAMPLE), ]
 }
 
 # Calculate distance to nearest and threshold
-db <- distToNearest(db, model="ham", first=FALSE, normalize="len", nproc=opt$NPROC)
-threshold <- findThreshold(db$DIST_NEAREST, method=opt$METHOD, model=opt$MODEL, subsample=opt$SUBSAMPLE)
+db <- distToNearest(db, model="ham", first=FALSE, normalize="len", nproc=NPROC)
 
-# Extract relevant slots into data_frame
-slots <- slotNames(threshold)
-slots <- slots[!(slots %in% c("x", "xdens", "ydens"))]
-.extract <- function(x) {
-    data_frame(PARAMETER=x, VALUE=as.character(slot(threshold, x)))
+
+# Generate thresh_df containing threshold parameters from repeats
+threshold_list <- list()
+
+
+# Also plot
+plot_file <- file.path(OUTDIR, paste0(NAME, "_threshold-plot.pdf"))
+pdf(plot_file, width=6, height=4, useDingbats=FALSE)
+
+for(i in 1:REPEATS){
+    threshold <- findThreshold(sample(db$DIST_NEAREST, TSUBSAMPLE), method=METHOD, model=MODEL, subsample=SUBSAMPLE)
+    
+    slots <- slotNames(threshold)
+    slots <- slots[!(slots %in% c("x", "xdens", "ydens"))]
+    .extract <- function(x) {
+        return(data_frame(PARAMETER=x, VALUE=as.character(slot(threshold, x))))
+    }
+    
+    threshold_list[[as.character(i)]] <- bind_rows(lapply(slots, .extract))
+    
+    plot(threshold, binwidth=0.02, silent=FALSE)
 }
-thresh_df <- bind_rows(lapply(slots, .extract))
+
+thresh_df <- bind_rows(threshold_list, .id = "REPEAT") %>%
+    spread(PARAMETER, VALUE) %>%
+    select(-REPEAT)
+
+dev.off()
+
 
 # Print and save threshold table
-cat("THRESHOLD> ", threshold@threshold, "\n", sep="")
-write_tsv(thresh_df, file.path(opt$OUTDIR, paste0(opt$NAME, "_threshold-values.tab")))
-
-# Plot
-plot_file <- file.path(opt$OUTDIR, paste0(opt$NAME, "_threshold-plot.pdf"))
-pdf(plot_file, width=6, height=4, useDingbats=FALSE)
-plot(threshold, binwidth=0.02, silent=FALSE)
-dev.off()
+cat("THRESHOLD_AVG > ", mean(as.numeric(thresh_df$threshold)), "\n", sep="")
+write_tsv(thresh_df, file.path(OUTDIR, paste0(NAME, "_threshold-values.tab")))
