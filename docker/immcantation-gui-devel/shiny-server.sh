48b004022c3a10ddd62ac0298d4977f8af821683
ssnn <ssnn@localhost>
2018-09-07 13:19:21 -0400

    Path changes

--- docker/immcantation-gui-devel/shiny-server.sh
+++ docker/immcantation-gui-devel/shiny-server.sh
@@ -1,10 +1,3 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# Create log dir
-mkdir -p /var/log/shiny-server
-chown shiny.shiny /var/log/shiny-server
-
-mkdir -p /var/lib/shiny-server/bookmarks
-chown shiny.shiny /var/lib/shiny-server/bookmarks
-
-exec shiny-server >> /var/log/shiny-server.log 2>&1 
+exec shiny-server >> /var/log/shiny-server/shiny-server.log 2>&1 
