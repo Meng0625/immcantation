3a740c9f1816fd8d02868ca1d3fcb42871228ff5
ruoyijiangyale <ruoyi.jiang@yale.edu>
2019-03-22 13:58:00 -0400

    addition of _10X and upper for merge10x.py and bug (missing import sys)

--- scripts/merge10x.py
+++ scripts/merge10x.py
@@ -5,6 +5,7 @@ Join the 10X annotation table and Change-O table
 
 # Imports
 import pandas as pd
+import sys
 
 # Parse arguments
 changeo_file = sys.argv[1]
@@ -12,6 +13,9 @@ annotation_file = sys.argv[2]
 out_file = sys.argv[3]
 
 # Merge annotations
+annfile = pd.read_csv(annotation_file).set_index('contig_id')
+annfile.columns = [column.upper() + '_10X' for column in annfile.columns]
+
 pd.read_csv(changeo_file, dtype = 'object', sep = '\t').set_index('SEQUENCE_ID')\
-.join(pd.read_csv(annotation_file).set_index('contig_id'), lsuffix='', rsuffix='_10X')\
+.join(annfile, lsuffix='', rsuffix='_10X')\
 .to_csv(out_file, sep = '\t')
